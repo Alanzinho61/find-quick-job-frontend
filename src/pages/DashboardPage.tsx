69b4d749c5ffb3f802adbfdba0f2ae1a96c552f9
Mücahit Yücel <82612069+Alanzinho61@users.noreply.github.com>
2025-05-28 23:10:44 +0300

    created Dashboard Page

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+import { useAuthStore } from '../store/authStore';
+
+const DashboardPage = () => {
+    const user = useAuthStore((state) => state.user);
+
+    return (
+        <Box>
+            <Typography variant="h4" gutterBottom>
+                Welcome to FindQuickJob 👋
+            </Typography>
+            {user && (
+                <Typography variant="h6">
+                    Logged in as: {user.name} ({user.email})
+                </Typography>
+            )}
+        </Box>
+    );
+};
+
+export default DashboardPage;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,33 +1,36 @@
-import { Routes, Route, Router, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/auth/LoginPage';
 import RegisterPage from '../pages/auth/RegisterPage';
-import JobListPage from '../pages/job/JobListPage';
-//import MainLayout from '../layouts/MainLayout';
-//import AuthLayout from '../layouts/AuthLayout';
+import MainLayout from '../layouts/MainLayout';
+import AuthLayout from '../layouts/AuthLayout';
 import RouteGuard from './RouteGuard';
-import DashboardPage from '../layouts/DashboardPage';
+import DashboardPage from '../pages/DashboardPage';
 
 const AppRoutes = () => {
     return (
         <Routes>
+            {/* AUTH ROUTES */}
             <Route path="/auth" element={<AuthLayout />}>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
+                <Route index element={<Navigate to="login" />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="register" element={<RegisterPage />} />
             </Route>
 
+            {/* PROTECTED ROUTES */}
             <Route
                 path="/"
                 element={
                     <RouteGuard>
                         <MainLayout />
                     </RouteGuard>
-                }>
-
+                }
+            >
                 <Route index element={<DashboardPage />} />
             </Route>
+
+            {/* 404 fallback */}
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-
     );
 };
 
